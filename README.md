9becc58849265c54a874195d1c4a826982f4aa03
Mider <mider.wang@gmail.com>
2019-03-15 17:17:33 +0800

    1、Add channel dev/stable 2、Update README 3、Update shell

--- README.md
+++ README.md
@@ -1,28 +1,60 @@
 # Flutter
 
-Currently installs flutter latest stable version in flutter/flutter github (https://github.com/flutter/flutter)
+Homebrew shell for Flutter in flutter/flutter github (https://github.com/flutter/flutter)
 
 ## Installation:
+
+### ①、Install stable channel:
 
 ```shell
 $ brew tap MiderWong/flutter
 $ brew install flutter
 ```
 
+### ②、Install devel channel:
+
+```shell
+$ brew tap MiderWong/flutter
+$ brew install --devel flutter
+```
+
 ## Upgrade
 
+### ①、Upgrade stable channel:
 ```shell
-$ brew upgrade flutter
-```
-or
-```shell
-$ flutter upgrade
+$ brew reinstall flutter
 ```
 
+### ②、Upgrade devel channel:
+
+```shell
+$ brew reinstall --devel flutter
+```
+
+## Change channel
+
+### ①、Change stable to dev:
+
+```shell
+$ brew uninstall --force flutter
+$ rm -rf "$(brew --cache)/flutter--git"
+$ brew install --devel flutter
+```
+
+### ②、Change dev to stable:
+
+```shell
+$ brew uninstall --force flutter
+$ rm -rf "$(brew --cache)/flutter--git"
+$ brew install flutter
+```
+     
 ## Uninstallation:
 
 ```shell
-$ brew uninstall flutter
+$ brew uninstall --force flutter
+$ rm -rf "$(brew --cache)/flutter--git"
+$ brew untap MiderWong/flutter
 ```
 
 ## Post-Installation:
@@ -39,10 +71,23 @@ If you're located in China, please follow：
 
   https://github.com/flutter/flutter/wiki/Using-Flutter-in-China
 
-After installed,run the following command to see if there are any platform dependencies you need to complete the setup:
+After installed , please set `PUB_HOSTED_URL` & `FLUTTER_STORAGE_BASE_URL`
 
+You may wish to add the flutter-ROOT install location to your PATH:
 
-  ```shell
-  $ flutter doctor
-  ```
+```shell
+$ echo 'export PATH="/usr/local/opt/flutter/bin:$PATH"' >> ~/.zshrc
+```
+
+You can use the following command to show flutter version:
+
+```shell
+$ flutter --version
+```
+
+Run the following command to see if there are any platform dependencies you need to complete the setup:
+
+```shell
+$ flutter doctor
+```
   
--- flutter.rb
+++ flutter.rb
@@ -1,18 +1,31 @@
 class Flutter < Formula
-  require "net/http"
-  desc "Homebrew shell for Flutter stable"
+  require "nokogiri"
+  require "open-uri"
+  desc "Homebrew shell for Flutter"
   homepage "https://flutter.io"
-  url "https://github.com/flutter/flutter.git", :branch => "stable"
-  version "1.0.0"
+
+  stable do
+    url "https://github.com/flutter/flutter.git", :branch => "stable"
+    version "stable"
+  end
+
+  devel do
+    url "https://github.com/flutter/flutter.git", :branch => "dev"
+    version "dev"
+  end
+
   bottle :unneeded
 
   def install
-    uri = URI("http://www.cip.cc")
-    res = Net::HTTP.get_response(uri)
-    # Body
-    respbody = res.body.force_encoding("utf-8")
-    addcn = respbody.include?("中国".force_encoding("utf-8"))
-    if addcn == true
+    document = Nokogiri::HTML.parse(open("http://www.cip.cc"))
+    ipaddress = document.xpath("//div[contains(@class, 'data')]//pre").text.force_encoding("utf-8")
+    ipaddress_cn = ipaddress.include?("中国".force_encoding("utf-8"))
+    ipaddress_macao = ipaddress.include?("澳门".force_encoding("utf-8"))
+    ipaddress_hk = ipaddress.include?("香港".force_encoding("utf-8"))
+    ipaddress_taiwan = ipaddress.include?("台湾".force_encoding("utf-8"))
+
+    if (ipaddress_cn == true) && (ipaddress_macao == false) && (ipaddress_hk == false) && (ipaddress_taiwan == false)
+      opoo "You are located in China"
       ENV["PUB_HOSTED_URL"] = "https://pub.flutter-io.cn"
       ENV["FLUTTER_STORAGE_BASE_URL"] = "https://storage.flutter-io.cn"
     end
@@ -29,6 +42,19 @@ class Flutter < Formula
 
   def caveats
     <<~EOS
+      Remove the proxy settings for command-line before you begin.
+
+      Run the following command to install stable channel:
+        brew install flutter
+
+      Run the following command to install dev channel:
+        brew install --devel flutter
+
+      If you want to change channel,please run the following command:
+        brew uninstall --force flutter
+        rm -rf "$(brew --cache)/flutter--git"
+        brew install (--devel) flutter
+
       If you're located in China, please follow：
         https://github.com/flutter/flutter/wiki/Using-Flutter-in-China
 
@@ -37,9 +63,14 @@ class Flutter < Formula
       You may wish to add the flutter-ROOT install location to your PATH:
         echo 'export PATH="/usr/local/opt/flutter/bin:$PATH"' >> ~/.zshrc
 
+      You can use the following command to show flutter version:
+        flutter --version
+
       Run the following command to see if there are any platform dependencies you need to complete the setup:
         flutter doctor
 
+      Run the following command to upgrade flutter:
+        brew reinstall (--devel) flutter
     EOS
   end
 
